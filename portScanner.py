ecde050a8c7fea6d8247295e0c154525c85d7091
Finegan <126989239+fineg4n@users.noreply.github.com>
2023-03-12 22:36:53 -0300

    Create portScanner.py

---
+++ portScanner.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import nmap
+import os
+
+def welcome():
+    welc = 'NMAP Port Scanner'
+    info = '[INFO] Python script to scan ports with NMAP by F1neg4n\n'
+    os.system('clear')
+    print(welc + '\n' + '*' * len(welc))
+    print(info)
+    return
+
+def getAddress():
+    welcome()
+    try:
+        ip = input('[+] Enter the IP Address: ')
+        while(ip == ''):
+            ip = input('[+] Please, enter the IP Address: ')
+    except(KeyboardInterrupt):
+        os.system('clear')
+        welcome()
+        print('[-] Interrupted by user!')
+        exit()
+    return ip
+
+def scannerNmap():
+    try:
+        host = getAddress()
+        nm = nmap.PortScanner()
+        results = nm.scan(host)
+        os.system('clear')
+        welcome()
+        for host in nm.all_hosts():
+            print('Host\t: %s' % (host))
+            print('State\t: %s' % nm[host].state())
+            for proto in nm[host].all_protocols():
+                print('------------')
+                print('Protocol : %s' % proto)
+                lport = nm[host][proto].keys()
+                sorted(lport)
+                for port in lport:
+                    print('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+    except(KeyboardInterrupt):
+        print('[-] Interrupted by user!')
+    return
+
+if __name__ == '__main__':
+    scannerNmap()
